53f66d52c7a8d12604b5cb33672f3ca7dd4b9473
eduardoarce1807 <eduardoarce@upeu.edu.pe>
2021-12-15 05:55:39 -0500

    Puerto cambiado

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var moongose = require('mongoose');
 var application = require('./application');
+const PORT = process.env.PORT || 5000
 
 moongose.connect('mongodb+srv://eduardoarce1807:jmalpaca@cluster0.tlnxn.mongodb.net/JMAlpaca?retryWrites=true&w=majority', (err, res) =>{
     if(err){
@@ -10,7 +11,7 @@ moongose.connect('mongodb+srv://eduardoarce1807:jmalpaca@cluster0.tlnxn.mongodb.
     else{
         console.log('Se ha conectado con la BBDD correctamente :D');
 
-        application.listen(8292, function(){
+        application.listen(PORT, function(){
             console.log("El servidor web se ha iniciado correctamente");
         });
 
